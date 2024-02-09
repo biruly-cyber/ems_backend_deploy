c88765943607001eabb6b8f04f4d9b6b683c8314
biruly-cyber <biruly2000@gmail.com>
2024-02-09 15:03:22 +0530

    add connecton

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 import { app } from "./app.js";
-import mongoose from "mongoose";
+import { connectDB } from "./configuration/database.js";
 // define port with default port 5000
 const PORT = process.env.PORT || 5000
 
 
-// Connect to the database
-const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URL, {
-        dbName: "employee_crm",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("Employee CRM Database Connected");
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1); // Exit process with failure
-    }
-  };
-  
+
   // Call the connectDB function
   connectDB();
   
