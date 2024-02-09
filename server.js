e54fecac88950200a5bd7ccd648a6673c12ec7ac
BHAGWAN BIRULY <64977401+biruly-cyber@users.noreply.github.com>
2024-02-09 15:14:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000
 
   // Call the connectDB function
   connectDB();
+
+app.get('/check-db-connection', (req, res) => {
+  const isConnected = mongoose.connection.readyState === 1; // 1 means connected
+  res.json({ connected: isConnected });
+});
+
   
   // Start the server
   app.listen(PORT, () => {
